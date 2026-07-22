da5767bf100d8546ba5c2977e53f36658b1eaad4
aNiket-290 <aniketchourey01@gmail.com>
2026-07-22 21:59:52 +0530

    Time: 20 ms (35.88%), Space: 112 MB (5.17%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        boolean ans = false;
+        for(int i=0;i<nums.length;i++){
+            if (map.containsKey(nums[i])) {
+                ans = true;
+                break;
+            }
+            else{
+                map.put(nums[i],i);
+            }
+        }
+        return ans;
+    }
+}
