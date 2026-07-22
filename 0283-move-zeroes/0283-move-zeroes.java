3432d9d41ca961188c96616b47c684a35956b0db
aNiket-290 <aniketchourey01@gmail.com>
2026-07-22 22:28:17 +0530

    Time: 2 ms (92.14%), Space: 47.7 MB (76.02%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,13 +1,27 @@
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         int n=nums.length;
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if(nums[i]==0&&nums[j]!=0){
+//                     nums[i] = nums[j];
+//                     nums[j] = 0;
+//                 }
+//             }
+//         }
+//     }
+// }
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]==0&&nums[j]!=0){
-                    nums[i] = nums[j];
-                    nums[j] = 0;
-                }
+        int index =0;
+        for(int i =0;i< nums.length;i++){
+            if(nums[i]!=0){
+                nums[index]=nums[i];
+                index++;
             }
+        }
+        for(int i=index;i<nums.length;i++){
+            nums[i]=0;
         }
     }
 }
