4a7d4aacc1fe9252191b3a7ff02fe1f980260193
aNiket-290 <aniketchourey01@gmail.com>
2026-07-22 22:25:56 +0530

    Time: 285 ms (5.07%), Space: 47.9 MB (20.34%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==0&&nums[j]!=0){
+                    nums[i] = nums[j];
+                    nums[j] = 0;
+                }
+            }
+        }
+    }
+}
