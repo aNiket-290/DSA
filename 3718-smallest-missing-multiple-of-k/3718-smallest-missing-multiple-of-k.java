50ecf774688d954f886d740267c1e5d52a3630e1
aNiket-290 <aniketchourey01@gmail.com>
2026-08-25 19:41:44 +0530

    Time: 2 ms (68.2%), Space: 44.2 MB (99.87%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -3,9 +3,11 @@ class Solution {
         int n = nums.length;
         HashSet<Integer> set = new HashSet<>();
         for(int i=0;i<n;i++){
-            set.add(nums[i]);
+            if(nums[i]%k==0){
+                set.add(nums[i]);
+            }
         }
-        for(int i=1;i<=101;i++){
+        for(int i=1;i<=set.size()+1;i++){
             if(!set.contains(i*k)) return i*k;
         }
         return 0;
